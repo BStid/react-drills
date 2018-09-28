a3e1fe915dce0de4ac5819e44e15415296d53d08
Ben <ben@benstidham.com>
2018-09-28 15:25:44 -0500

    Afternoon 3 Comnpleted

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      list: ["Boy", "Girl", "Cat", "Dog", "Fish", "Pizza", "Punk"]
+    };
+  }
+  handleChange(val) {
+    this.setState({ input: val.toLowerCase() });
+  }
+
   render() {
+    console.log(this.state.input);
+    // this.state.list = this.state.list.map((value, index) => {
+    //   return value.toLowerCase();
+    // });
+    let filteredList = this.state.list
+      .filter(value => {
+        return value.toLowerCase().includes(this.state.input);
+      })
+      .map((value, index) => {
+        return <h1 key={index}>{value}</h1>;
+      });
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          type="text"
+          onChange={element => this.handleChange(element.target.value)}
+        />
+        {filteredList}
       </div>
     );
   }
