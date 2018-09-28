15d516c82c561d9b0a467e9b386ceb5adf0d2bb9
Ben <ben@benstidham.com>
2018-09-28 15:24:50 -0500

    Afternoon 2 Complete

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,20 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: ["One", "Two", "Three", "Four", "Five"]
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    let listOnum = this.state.list.map((value, index) => {
+      return (
+        <h1 key={index}>
+          {index}: {value}
+        </h1>
+      );
+    });
+    return <div>{listOnum}</div>;
   }
 }
 
